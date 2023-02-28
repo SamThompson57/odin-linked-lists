73113197925c6b12a543deca4082e3a5a2251781
Pidgicle <s_thomo@hotmail.co.uk>
2023-02-28 20:55:40 +0000

    removed needless assignment comments.

--- linkedlists.js
+++ linkedlists.js
@@ -1,27 +1,3 @@
-/*
-
-find(value) - returns the index of the node 
-containing value, or null if not found.
-
-toString - represents your LinkedList objects as 
-strings, so you can print them out and preview 
-them in the console. The format should be: 
-( value ) -> ( value ) -> ( value ) -> null
-
-Extra Credit
-
-insertAt(value, index) - that inserts a new node 
-with the provided value at the given index.
-
-removeAt(index) - that removes the node at the 
-given index.
-
-Extra Credit Tip: When you insert or remove a 
-node, consider how it will affect the existing 
-nodes. Some of the nodes will need their 
-nextNode link updated.
-*/
-
 const node = (value = null, nextNode = null ) => {
     return {value, nextNode}
 }
